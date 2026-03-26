2401214463d69c67dfb8dca88fea3bfa3d57dd4d
Chris <L25171185@culiacan.tecnm.mx>
2026-03-25 23:16:40 -0700

    Falta trabajo

--- MathLingo/frontend/src/App.jsx
+++ MathLingo/frontend/src/App.jsx
@@ -1,121 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { useState, useRef } from "react"   //Metodo hook de react 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+  //Primero se crean los estados de las cosas que usaremos
+  const [num1, setNum1] = useState(0)    //Primero declaras y lo segundo es con lo que alteras su valor
+  const [num2, setNum2] = useState(0)    //useState es lo que uso para darle valor a las cosas
+  const [respuesta, setRespuesta] = useState('')
+  const [mensaje, setMensaje] = useState('')
+  const [animacion, setAnimacion] = useState(false)
 
-      <div className="ticks"></div>
+  const lottieRef = useRef(null);   //Es con lo que usaremos las animaciones, useRef
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+  //Funcion para comprobar respuesta
+  const comprobarRespuesta = () => {
+    if (parseInt(respuesta) === num1 + num2) {
+      setMensaje('Correcto tilin');
+      setMostrarCelebracion(true);
+    } else {
+      setMensaje('Hash fallado')
+      setTimeout(() => {
+          generarNuevaPregunta();
+          setMostrarCelebracion(false);
+      }, 2000);
+    }
+  }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  //Funcion para generarPreguntaNueva
+  const generarNuevaPregunta = () => {
+    setNum1(Math.floor(Math.random() * 10));
+    setNum2(Math.floor(Math.random() * 10));
+    setRespuesta('');
+    setMensaje('Cuanto es?');
+  };
+
 }
-
-export default App
