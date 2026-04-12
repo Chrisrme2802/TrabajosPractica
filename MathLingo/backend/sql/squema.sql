ad65d95f57d73a5321752528c54c243ce840594b
Chris <L25171185@culiacan.tecnm.mx>
2026-04-11 20:42:27 -0700

    Falta arreglar google

--- MathLingo/backend/sql/squema.sql
+++ MathLingo/backend/sql/squema.sql
@@ -3,9 +3,10 @@ CREATE TABLE IF NOT EXISTS usuarios (
     google_ID VARCHAR(255) PRIMARY KEY,
     nombre VARCHAR(100),
     foto_url TEXT,
-    rango VARCHAR(50) DEFAULT 'Principiante',
     monedas INTEGER DEFAULT 0,
     experiencia INTEGER DEFAULT 0,
     nivel_desbloqueado INTEGER DEFAULT 1,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    racha_actual INTEGER DEFAULT 0,
+    ultima_conexion DATE DEFAULT CURRENT_DATE
 );
